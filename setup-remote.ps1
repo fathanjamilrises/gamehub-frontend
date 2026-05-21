f0c7cee511e82471984fd5434d34370d275d9771
FathhJynx <fathanjamil757@gmail.com>
2026-05-21 12:34:04 +0700

    chore: tambah script setup remote fathan

---
+++ setup-remote.ps1
@@ -0,0 +1,4 @@
+git remote add fathan https://github.com/fathanjamilrises/gamehub-frontend.git
+git fetch fathan
+Write-Host "Remote 'fathan' berhasil ditambahkan." -ForegroundColor Green
+git remote -v
